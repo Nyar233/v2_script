741dad4e4db9c0e071785f0b69b3a441c7556891
coffee <nyar_233@outlook.com>
2020-10-16 19:18:22 +0800

    Update shell1.sh

--- shell1.sh
+++ shell1.sh
@@ -58,6 +58,7 @@ esac
 
 config_tls(){
     read -p "input your domain: " domain_tls
+    read -p "input your UUID: " UUID_v2
     useradd -s /usr/sbin/nologin v2ray
     install -d -o v2ray -g v2ray /etc/ssl/v2ray/
     install -m 644 -o v2ray -g v2ray /etc/letsencrypt/live/$domain_tls/fullchain.pem -t /etc/ssl/v2ray/
@@ -75,12 +76,155 @@ EOF
 
 chmod +x /etc/letsencrypt/renewal-hooks/deploy/v2ray.sh
 
+cat > /usr/local/etc/v2ray/config.json <<EOF
+{
+    "log": {
+        "loglevel": "warning"
+    },
+    "inbounds": [
+        {
+            "port": 443,
+            "protocol": "vless",
+            "settings": {
+                "clients": [
+                    {
+                        "id": "$UUID_v2", // 填写你的 UUID
+                        "flow": "xtls-rprx-direct",
+                        "level": 0,
+                        "email": "love@v2fly.org"
+                    }
+                ],
+                "decryption": "none",
+                "fallbacks": [
+                    {
+                        "dest": 80 // 或者回落到其它也防探测的代理
+                    },
+                    {
+                        "path": "/vlws", // 必须换成自定义的 PATH
+                        "dest": 1234,
+                        "xver": 1
+                    },
+                    {
+                        "path": "/vmtcp", // 必须换成自定义的 PATH
+                        "dest": 2345,
+                        "xver": 1
+                    },
+                    {
+                        "path": "/vmws", // 必须换成自定义的 PATH
+                        "dest": 3456,
+                        "xver": 1
+                    }
+                ]
+            },
+            "streamSettings": {
+                "network": "tcp",
+                "security": "xtls",
+                "xtlsSettings": {
+                    "alpn": [
+                        "http/1.1"
+                    ],
+                    "certificates": [
+                        {
+                            "certificateFile": "/etc/ssl/v2ray/fullchain.pem", // 换成你的证书，绝对路径
+                            "keyFile": "/etc/ssl/v2ray/privkey.pem" // 换成你的私钥，绝对路径
+                        }
+                    ]
+                }
+            }
+        },
+        {
+            "port": 1234,
+            "listen": "127.0.0.1",
+            "protocol": "vless",
+            "settings": {
+                "clients": [
+                    {
+                        "id": "$UUID_v2", // 填写你的 UUID
+                        "level": 0,
+                        "email": "love@v2fly.org"
+                    }
+                ],
+                "decryption": "none"
+            },
+            "streamSettings": {
+                "network": "ws",
+                "security": "none",
+                "wsSettings": {
+                    "acceptProxyProtocol": true, // 提醒：若你用 Nginx/Caddy 等反代 WS，需要删掉这行
+                    "path": "/vlws" // 必须换成自定义的 PATH，需要和分流的一致
+                }
+            }
+        },
+        {
+            "port": 2345,
+            "listen": "127.0.0.1",
+            "protocol": "vmess",
+            "settings": {
+                "clients": [
+                    {
+                        "id": "$UUID_v2", // 填写你的 UUID
+                        "level": 0,
+                        "email": "love@v2fly.org"
+                    }
+                ]
+            },
+            "streamSettings": {
+                "network": "tcp",
+                "security": "none",
+                "tcpSettings": {
+                    "acceptProxyProtocol": true,
+                    "header": {
+                        "type": "http",
+                        "request": {
+                            "path": [
+                                "/vmtcp" // 必须换成自定义的 PATH，需要和分流的一致
+                            ]
+                        }
+                    }
+                }
+            }
+        },
+        {
+            "port": 3456,
+            "listen": "127.0.0.1",
+            "protocol": "vmess",
+            "settings": {
+                "clients": [
+                    {
+                        "id": "$UUID_v2", // 填写你的 UUID
+                        "level": 0,
+                        "email": "love@v2fly.org"
+                    }
+                ]
+            },
+            "streamSettings": {
+                "network": "ws",
+                "security": "none",
+                "wsSettings": {
+                    "acceptProxyProtocol": true, // 提醒：若你用 Nginx/Caddy 等反代 WS，需要删掉这行
+                    "path": "/vmws" // 必须换成自定义的 PATH，需要和分流的一致
+                }
+            }
+        }
+    ],
+    "outbounds": [
+        {
+            "protocol": "freedom"
+        }
+    ]
+}
+EOF
+systemctl restart v2ray
+
 echo "done."
 echo '"certificateFile": "/etc/ssl/v2ray/fullchain.pem"'
 echo '"keyFile": "/etc/ssl/v2ray/privkey.pem"'
+echo '---------------------------------------------'
+echo "do it by yourself:"
 echo "systemctl edit v2ray.service"
 echo "[Service]"
 echo "User=v2ray"
+echo "systemctl restart v2ray"
 }
 # install v2ray
 v2_install(){
